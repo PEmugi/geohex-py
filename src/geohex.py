cdba7f5fb7bc6156c084007abe52cd5fa706653c
Shinpei Matsuura <pe@chizuwota.net>
2010-04-06 02:59:27 +0900

    class

--- src/geohex.py
+++ src/geohex.py
@@ -29,12 +29,12 @@ class GeoHexFactory(object):
         '''Get GeoHex Object by GeoHex Code'''
         pass
 
-    def get_hexcode(self, lon, lat, level=7):
+    def __get_hexcode(self, lon, lat, level=7):
         h_size = level * self.locator['h_size']
         lon_grid = lon * self.locator['h_grid']
         lat_grid = lat * self.locator['h_grid']
-        unit_x = 6 * self.locator['h_size']
-        unit_y = 2.8 * self.locator['h_size']
+        unit_x = 6 * h_size
+        unit_y = 2.8 * h_size
         h_k = (round((1.4/3) * self.locator['h_grid']))/self.locator['h_grid']
         base_x = math.floor((self.locator['min_x_lon'] + self.locator['min_x_lat'] / h_k) / unit_x)
         base_y = math.floor((self.locator['min_y_lat'] - h_k * self.locator['min_y_lon']) / unit_y)
@@ -66,11 +66,11 @@ class GeoHexFactory(object):
 
         if level:
             if level < 7:
-                hexcode = h_key[level%60] + h_key[int(h_x_100)] + h_key[int(h_y_100)] + h_key[int(h_x_10)] + h_key[int(h_y_10)] + h_key[int(h_x_1)] + h_key[int(h_y_1)]
+                hexcode = __H_KEY[level%60] + __H_KEY[int(h_x_100)] + __H_KEY[int(h_y_100)] + __H_KEY[int(h_x_10)] + __H_KEY[int(h_y_10)] + __H_KEY[int(h_x_1)] + __H_KEY[int(h_y_1)]
             else:
-                hexcode = h_key[level%60] + h_key[int(h_x_10)] + h_key[int(h_y_10)] + h_key[int(h_x_1)] + h_key[int(h_y_1)]
+                hexcode = __H_KEY[level%60] + __H_KEY[int(h_x_10)] + __H_KEY[int(h_y_10)] + __H_KEY[int(h_x_1)] + __H_KEY[int(h_y_1)]
         else:
-            hexcode = h_key[int(h_x_10)] + h_key[int(h_y_10)] + h_key[int(h_x_1)] + h_key[int(h_y_1)]
+            hexcode = __H_KEY[int(h_x_10)] + __H_KEY[int(h_y_10)] + __H_KEY[int(h_x_1)] + __H_KEY[int(h_y_1)]
 
         return hexcode
 
@@ -89,8 +89,49 @@ class GeoHex(object):
 
 
 
-
 #utility methods
 
-
-
+#def latlon2geohex(lat, lon, level=7, locator=__DEFAULT_LOCATOR):
+#    h_size = level * locator['h_size']
+#    lon_grid = lon * locator['h_grid']
+#    lat_grid = lat * locator['h_grid']
+#    unit_x = 6 * locator['h_size']
+#    unit_y = 2.8 * locator['h_size']
+#    h_k = (round((1.4/3) * locator['h_grid']))/locator['h_grid']
+#    base_x = math.floor((locator['min_x_lon'] + locator['min_x_lat'] / h_k) / unit_x)
+#    base_y = math.floor((locator['min_y_lat'] - h_k * locator['min_y_lon']) / unit_y)
+#    h_pos_x = (lon_grid + lat_grid/h_k) / unit_x - base_x
+#    h_pos_y = (lat_grid - h_k * lon_grid) / unit_y - base_y
+#    h_x_0 = math.floor(h_pos_x)
+#    h_y_0 = math.floor(h_pos_y)
+#    h_x_q = math.floor((h_pos_x - h_x_0) * 100.0) / 100.0
+#    h_y_q = math.floor((h_pos_y - h_y_0) * 100.0) / 100.0
+#    h_x = round(h_pos_x)
+#    h_y = round(h_pos_y)
+#
+#    if h_y_q > -h_x_q+1:
+#        if (h_y_q < 2*h_x_q) and (h_y_q>0.5*h_x_q):
+#            h_x = h_x_0 + 1
+#            h_y = h_y_0 + 1
+#
+#    elif h_y_q < -h_x_q+1:
+#        if (h_y_q > 2*h_x_q-1) and (h_y_q < 0.5*h_x_q+0.5):
+#            h_x = h_x_0
+#            h_y = h_y_0
+#
+#    h_x_100 = math.floor(h_x / 3600.0)
+#    h_x_10 = math.floor(h_x % 3600.0) / 60.0
+#    h_x_1 = math.floor(h_x % 3600.0) % 60.0
+#    h_y_100 = math.floor(h_y / 3600.0)
+#    h_y_10 = math.floor(h_y % 3600.0) / 60.0
+#    h_y_1 = math.floor(h_y % 3600.0) % 60.0
+#
+#    if level:
+#        if level < 7:
+#            hexcode = h_key[level%60] + h_key[int(h_x_100)] + h_key[int(h_y_100)] + h_key[int(h_x_10)] + h_key[int(h_y_10)] + h_key[int(h_x_1)] + h_key[int(h_y_1)]
+#        else:
+#            hexcode = h_key[level%60] + h_key[int(h_x_10)] + h_key[int(h_y_10)] + h_key[int(h_x_1)] + h_key[int(h_y_1)]
+#    else:
+#        hexcode = h_key[int(h_x_10)] + h_key[int(h_y_10)] + h_key[int(h_x_1)] + h_key[int(h_y_1)]
+#
+#    return hexcode
